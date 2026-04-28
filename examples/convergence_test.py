33ce5615fec7814e72341075d0897aa7c1764146
gioanateora <gioana.teora@polito.it>
2026-04-28 15:58:45 +0200

    add convergence test

--- examples/convergence_test.py
+++ examples/convergence_test.py
@@ -99,182 +99,64 @@ dirpath_pcc_2d = "./Export/Export_PCC_2D"
 if os.path.exists(dirpath_pcc_2d) and os.path.isdir(dirpath_pcc_2d):
     shutil.rmtree(dirpath_pcc_2d)
 
-dirpath_pcc_3d = "./Export/Export_PCC_3D"
-if os.path.exists(dirpath_pcc_3d) and os.path.isdir(dirpath_pcc_3d):
-    shutil.rmtree(dirpath_pcc_3d)
-
-dirpath_mcc_2d = "./Export/Export_MCC_2D"
-if os.path.exists(dirpath_mcc_2d) and os.path.isdir(dirpath_mcc_2d):
-    shutil.rmtree(dirpath_mcc_2d)
-
-execute_test_pcc = False
-execute_test_pcc_3d = True
-execute_test_mcc = False
 
 # Test PCC 2D
-if execute_test_pcc:
-    tol = 1.0e-12
-    test_type = 1
-    method_orders = [1, 2, 3]
-    method_types = [0, 1, 2, 3]
-    mesh_types = [0]
-    for mesh_type in mesh_types:
-        for method_type in method_types:
-            for order in method_orders:
-                export_path = dirpath_pcc_2d + "/Export_" +  str(method_type) + "_" +  str(order) + "_" + str(mesh_type)
-                os.system("python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.1 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.05 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.01 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.005 --export-path={2}".format(order, method_type, export_path, mesh_type))
 
-                errors = import_errors_pcc(export_path, method_type, order, test_type)
-                test_errors_pcc(errors, order, tol)
+tol = 1.0e-12
+test_type = 1
+method_orders = [1]
+method_types = [1, 4, 5]
+mesh_types = [0]
+for mesh_type in mesh_types:
+    for method_type in method_types:
+        for order in method_orders:
+            export_path = dirpath_pcc_2d + "/Export_" +  str(method_type) + "_" +  str(order) + "_" + str(mesh_type)
+            os.system("python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.1 --export-path={2}".format(order, method_type, export_path, mesh_type))
+            os.system("python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.05 --export-path={2}".format(order, method_type, export_path, mesh_type))
+            os.system("python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.01 --export-path={2}".format(order, method_type, export_path, mesh_type))
+            os.system("python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.005 --export-path={2}".format(order, method_type, export_path, mesh_type))
 
-    tol = 1.0e-12
-    test_type = 1
-    method_orders = [1]
-    method_types = [0, 1, 2, 3]
-    mesh_types = [5]
-    for mesh_type in mesh_types:
-        for method_type in method_types:
-            for order in method_orders:
-                export_path = dirpath_pcc_2d + "/Export_" +  str(method_type) + "_" +  str(order) + "_" + str(mesh_type)
-                os.system("python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.1 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.05 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.01 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.005 --export-path={2}".format(order, method_type, export_path, mesh_type))
+            errors = import_errors_pcc(export_path, method_type, order, test_type)
+            test_errors_pcc(errors, order, tol)
 
-                errors = import_errors_pcc(export_path, method_type, order, test_type)
-                test_errors_pcc(errors, order, tol)
+tol = 1.0e-12
+test_type = 1
+method_orders = [1]
+method_types = [1, 4, 5]
+mesh_types = [5]
+for mesh_type in mesh_types:
+    for method_type in method_types:
+        for order in method_orders:
+            export_path = dirpath_pcc_2d + "/Export_" +  str(method_type) + "_" +  str(order) + "_" + str(mesh_type)
+            os.system("python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.1 --export-path={2}".format(order, method_type, export_path, mesh_type))
+            os.system("python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.05 --export-path={2}".format(order, method_type, export_path, mesh_type))
+            os.system("python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.01 --export-path={2}".format(order, method_type, export_path, mesh_type))
+            os.system("python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.005 --export-path={2}".format(order, method_type, export_path, mesh_type))
 
-    tol = 1.0e-12
-    test_type = 1
-    method_orders = [1, 2, 3]
-    method_types = [1, 2, 3]
-    mesh_types = [2]
-    for mesh_type in mesh_types:
-        for method_type in method_types:
-            for order in method_orders:
-                export_path = dirpath_pcc_2d + "/Export_" + str(method_type) + "_" + str(order) + "_" + str(mesh_type)
-                os.system(
-                    "python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.1 --export-path={2}".format(
-                        order, method_type, export_path, mesh_type))
-                os.system(
-                    "python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.05 --export-path={2}".format(
-                        order, method_type, export_path, mesh_type))
-                os.system(
-                    "python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.01 --export-path={2}".format(
-                        order, method_type, export_path, mesh_type))
-                os.system(
-                    "python ./main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.005 --export-path={2}".format(
-                        order, method_type, export_path, mesh_type))
+            errors = import_errors_pcc(export_path, method_type, order, test_type)
+            test_errors_pcc(errors, order, tol)
 
-                errors = import_errors_pcc(export_path, method_type, order, test_type)
-                test_errors_pcc(errors, order, tol)
-
-# Test MCC 2D
-if execute_test_mcc:
-    tol = 1.0e-12
-    test_type = 1
-    method_orders = [0, 1, 2]
-    method_types = [1, 3, 6]
-    mesh_types = [0]
-    for mesh_type in mesh_types:
-        for method_type in method_types:
-            for order in method_orders:
-                export_path = dirpath_mcc_2d + "/Export_" +  str(method_type) + "_" +  str(order) + "_" + str(mesh_type)
-                os.system("python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.1 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.05 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.01 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.005 --export-path={2}".format(order, method_type, export_path, mesh_type))
-
-                errors = import_errors_mcc(export_path, method_type, order, test_type)
-                test_errors_mcc(errors, order, tol)
-
-    tol = 1.0e-12
-    test_type = 1
-    method_orders = [0, 1, 2]
-    method_types = [1, 3]
-    mesh_types = [5]
-    for mesh_type in mesh_types:
-        for method_type in method_types:
-            for order in method_orders:
-                export_path = dirpath_mcc_2d + "/Export_" +  str(method_type) + "_" +  str(order) + "_" + str(mesh_type)
-                os.system("python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.1 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.05 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.01 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.005 --export-path={2}".format(order, method_type, export_path, mesh_type))
-
-                errors = import_errors_mcc(export_path, method_type, order, test_type)
-                test_errors_mcc(errors, order, tol)
-
-    tol = 1.0e-12
-    test_type = 1
-    method_orders = [0, 1, 2]
-    method_types = [1, 3]
-    mesh_types = [2]
-    for mesh_type in mesh_types:
-        for method_type in method_types:
-            for order in method_orders:
-                export_path = dirpath_mcc_2d + "/Export_" + str(method_type) + "_" + str(order) + "_" + str(mesh_type)
-                os.system(
-                    "python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.1 --export-path={2}".format(
-                        order, method_type, export_path, mesh_type))
-                os.system(
-                    "python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.05 --export-path={2}".format(
-                        order, method_type, export_path, mesh_type))
-                os.system(
-                    "python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.01 --export-path={2}".format(
-                        order, method_type, export_path, mesh_type))
-                os.system(
-                    "python ./main_elliptic_mcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.005 --export-path={2}".format(
-                        order, method_type, export_path, mesh_type))
-
-                errors = import_errors_mcc(export_path, method_type, order, test_type)
-                test_errors_mcc(errors, order, tol)
-
-
-# Test PCC 3D
-
-if execute_test_pcc_3d:
-    tol = 1.0e-12
-    test_type = 1
-    method_orders = [1]
-    method_types = [0, 1, 2, 3]
-    mesh_types = [0]
-    for mesh_type in mesh_types:
-        for method_type in method_types:
-            for order in method_orders:
-                export_path = dirpath_pcc_3d + "/Export_" +  str(method_type) + "_" +  str(order) + "_" + str(mesh_type)
-                os.system("python ./main_elliptic_pcc_3d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-volume=0.005 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_pcc_3d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-volume=0.001 --export-path={2}".format(order, method_type, export_path, mesh_type))
-
-                # errors = import_errors_pcc(export_path, method_type, order, test_type)
-                # test_errors_pcc(errors, order, tol)
-                # errors = np.array(errors[1:])
-                # fig, ax = plt.subplots(figsize=(12, 12))
-                # ax.plot(errors[:, 0], errors[:, 1], '-k^', linewidth=2, markersize=12)
-                # plt.xlabel('$h$', fontsize=30)
-                # plt.ylabel('$e_0$', fontsize=30)
-                # plt.xticks(fontsize=20)
-                # plt.yticks(fontsize=20)
-                # plt.yscale('log')
-                # plt.xscale('log')
-                # plt.grid(True, which="both", ls="--")
-                # plt.ylim(None, 10)
-                # plt.show()
-
-    tol = 1.0e-12
-    test_type = 1
-    method_orders = [1]
-    method_types = [0, 1, 2, 3]
-    mesh_types = [6]
-    for mesh_type in mesh_types:
-        for method_type in method_types:
-            for order in method_orders:
-                export_path = dirpath_pcc_3d + "/Export_" +  str(method_type) + "_" +  str(order) + "_" + str(mesh_type)
-                os.system("python ./main_elliptic_pcc_3d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-volume=0.0025 --export-path={2}".format(order, method_type, export_path, mesh_type))
-                os.system("python ./main_elliptic_pcc_3d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-volume=0.0005 --export-path={2}".format(order, method_type, export_path, mesh_type))
-
-                errors = import_errors_pcc(export_path, method_type, order, test_type)
-                test_errors_pcc(errors, order, tol)
+# tol = 1.0e-12
+# test_type = 1
+# method_orders = [1]
+# method_types = [1, 5]
+# mesh_types = [2]
+# for mesh_type in mesh_types:
+#     for method_type in method_types:
+#         for order in method_orders:
+#             export_path = dirpath_pcc_2d + "/Export_" + str(method_type) + "_" + str(order) + "_" + str(mesh_type)
+#             os.system(
+#                 "python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.1 --export-path={2}".format(
+#                     order, method_type, export_path, mesh_type))
+#             os.system(
+#                 "python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.05 --export-path={2}".format(
+#                     order, method_type, export_path, mesh_type))
+#             os.system(
+#                 "python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.01 --export-path={2}".format(
+#                     order, method_type, export_path, mesh_type))
+#             os.system(
+#                 "python ./examples/main_elliptic_pcc_2d.py --method-order={0} --method-type={1} --test-id=1 --mesh-type={3} --mesh-max-relative-area=0.005 --export-path={2}".format(
+#                     order, method_type, export_path, mesh_type))
+#
+#             errors = import_errors_pcc(export_path, method_type, order, test_type)
+#             test_errors_pcc(errors, order, tol)
